7337da8c1448b1e8cce482e6676ed5c7fb55aa41
kirkl4nd <77247639+kirkl4nd@users.noreply.github.com>
2024-02-13 18:45:44 -0500

    Fixed print index

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Detected {} Helios DAC(s).", helios_dacs.len());
     for (index, device) in helios_dacs.iter().enumerate() {
         let device_desc = device.device_descriptor()?;
-        println!("Device {}: Vendor ID = {:04x}, Product ID = {:04x}", index + 1, device_desc.vendor_id(), device_desc.product_id());
+        println!("Device {}: Vendor ID = {:04x}, Product ID = {:04x}", index, device_desc.vendor_id(), device_desc.product_id());
     }
 
     Ok(())
